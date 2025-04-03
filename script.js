f37898cb0e4b854be8c8c3ac99ac3fc698752317
unknown <sapna.alex03@gmail.com>
2025-04-03 10:33:14 +0530

    saving_password_and_setting_username_in_cookie_assignment_1

--- script.js
+++ script.js
@@ -15,9 +15,27 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
+  let count = getCookie("count");
   // 2. If the cookie exists, increment the value and update the cookie
-  // 3. If the cookie does not exist, create it and set the value to 1
-  // 4. Display the count on the webpage
+  if (count){
+    count = parseInt(count)+1;
 
-  // your code here
+  }else{
+    count = 1;
+  }
+  // 3. If the cookie does not exist, create it and set the value to 1
+  setCookie("count", count, 7);
+  // 4. Display the count on the webpage
+  let countDisplay = document.getElementById("countDisplay");
+  let pageViewDisplay = document.getElementById("pageViewDisplay");
+
+  if (countDisplay) {
+    countDisplay.innerText = `Page Views: ${count}`;
+  }
+
+  if (pageViewDisplay){
+    pageViewDisplay.innerText = count;
+  }
+
+  
 });
